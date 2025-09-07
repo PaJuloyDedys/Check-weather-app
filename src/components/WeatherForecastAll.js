199185ef1a1510a21d420fb630fb472ea5453ce1
thelegend2004 <oleh.liubichenko@gmail.com>
2025-09-07 20:13:12 +0300

    fix: fix styles. view all week days. clean code a bit.

--- src/components/WeatherForecastAll.js
+++ src/components/WeatherForecastAll.js
@@ -1,42 +1,24 @@
 import WeatherForecastOneDay from "./WeatherForecastOneDay";
 
 export default function WeatherForecastAll({ weather, getIcon }) {
-  // console.log(weather?.forecast.forecastday[1].date);
 
   function getDayName(day) {
     const date = new Date(day);
     return date.toLocaleDateString("en-US", { weekday: "long" });
   }
 
-  // console.log(weather?.forecast.forecastday[1].day.condition.text);
-  // console.log(weather?.forecast.forecastday[2].day.condition.text);
-  // console.log(weather?.forecast.forecastday[3].day.condition.text);
-  // console.log(weather?.forecast.forecastday[4].day.condition.text);
-  // const getDay = getDayName(weather?.forecast.forecastday[1].date);
-  // console.log(getDay); ☁️ 🌤️ ☀️ 🌦️ ⛈️ 🌧️
-
+  const allForecast = weather?.forecast.forecastday;
   return (
     <div className="forecast">
-      <WeatherForecastOneDay
-        day={getDayName(weather?.forecast.forecastday[1].date)}
-        sun={getIcon(1)}
-        temp={weather?.forecast.forecastday[1].day.avgtemp_c}
-      />
-      <WeatherForecastOneDay
-        day={getDayName(weather?.forecast.forecastday[2].date)}
-        sun={getIcon(2)}
-        temp={weather?.forecast.forecastday[2].day.avgtemp_c}
-      />
-      <WeatherForecastOneDay
-        day={getDayName(weather?.forecast.forecastday[3].date)}
-        sun={getIcon(3)}
-        temp={weather?.forecast.forecastday[3].day.avgtemp_c}
-      />
-      <WeatherForecastOneDay
-        day={getDayName(weather?.forecast.forecastday[4].date)}
-        sun={getIcon(4)}
-        temp={weather?.forecast.forecastday[4].day.avgtemp_c}
-      />
+      {
+        allForecast.map((fc, i) =>
+        (<WeatherForecastOneDay
+          day={getDayName(fc.date)}
+          sun={getIcon(i + 1)}
+          temp={fc.day.avgtemp_c}
+        />)
+        )
+      }
     </div>
   );
 }
